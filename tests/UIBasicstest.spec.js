783e44f13c278e4bac5cd50546995f1a50c78f53
Demoriel Purnell <demorielpurnell@Demoriels-MBP.attlocal.net>
2022-08-31 20:19:50 -0500

    locators and clicks

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -6,11 +6,18 @@ test('Browser Context Playwright test', async ({browser})=>
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://www.demoblaze.com/")
+
 });
 
 test('Page Playwright test', async ({page})=>
 {
-    await page.goto("https://www.google.com/")
+    await page.goto("https://www.demoblaze.com/")
     console.log(await page.title());
-    await expect(page).toHaveTitle("Google");
+    await expect(page).toHaveTitle("STORE");
+    await page.locator("//*[contains(text(),'Laptops')]").click();
+    await page.locator("//a[@class='hrefch'][contains(text(),'MacBook Pro')]").click();
+    await expect(page.locator('//a[@class="hrefch"][contains(text(),"MacBook Pro")]')).toHaveText('MacBook Pro');
+
+
+    
 });
