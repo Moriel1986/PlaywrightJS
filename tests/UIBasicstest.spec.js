ac800fc4693adb9f20d9805767d8b3483fa6aa1b
Demoriel Purnell <demorielpurnell@Demoriels-MBP.attlocal.net>
2022-08-31 21:15:44 -0500

    code gen

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,23 +1,44 @@
 const {test, chromium, expect} = require('@playwright/test');
 
 
-test('Browser Context Playwright test', async ({browser})=>
-{
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://www.demoblaze.com/")
-
-});
-
 test('Page Playwright test', async ({page})=>
 {
-    await page.goto("https://www.demoblaze.com/")
-    console.log(await page.title());
-    await expect(page).toHaveTitle("STORE");
-    await page.locator("//*[contains(text(),'Laptops')]").click();
-    await page.locator("//a[@class='hrefch'][contains(text(),'MacBook Pro')]").click();
-    await expect(page.locator('//a[@class="hrefch"][contains(text(),"MacBook Pro")]')).toHaveText('MacBook Pro');
-
-
+  // Go to https://www.demoblaze.com/
+  await page.goto('https://www.demoblaze.com/');
+  // Click a:has-text("Log in")
+  await page.locator('a:has-text("Log in")').click();
+  // Click text=Log in × Username: Password: Close Log in >> input[type="text"]
+  await page.locator('text=Log in × Username: Password: Close Log in >> input[type="text"]').click();
+  // Fill text=Log in × Username: Password: Close Log in >> input[type="text"]
+  await page.locator('text=Log in × Username: Password: Close Log in >> input[type="text"]').fill('Moriel1986');
+  // Click text=Log in × Username: Password: Close Log in >> input[type="password"]
+  await page.locator('text=Log in × Username: Password: Close Log in >> input[type="password"]').click();
+  // Fill text=Log in × Username: Password: Close Log in >> input[type="password"]
+  await page.locator('text=Log in × Username: Password: Close Log in >> input[type="password"]').fill('Kobegoat#0824');
+  // Click button:has-text("Log in")
+  await page.locator('button:has-text("Log in")').click();
+  await expect(page).toHaveURL('https://www.demoblaze.com/');
+  // Click text=Laptops
+  await page.locator('text=Laptops').click();
+  await expect(page).toHaveURL('https://www.demoblaze.com/#');
+  // Click a:has-text("MacBook Pro")
+  await page.locator('a:has-text("MacBook Pro")').click();
+  await expect(page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=15');
+  // Click text=Add to cart
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.locator('text=Add to cart').click();
+  await expect(page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=15#');
+  // Click text=Home (current)
+  await page.locator('text=Home (current)').click();
+  await expect(page).toHaveURL('https://www.demoblaze.com/index.html');
     
 });
+
+
+
+
+
+
