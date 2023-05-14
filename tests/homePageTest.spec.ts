9ea5018da77923f7bcc16dd545cd81c4571941da
Demoriel Purnell <demorielpurnell@Demoriels-MBP.attlocal.net>
2023-05-14 10:43:26 -0500

    demoblaze homepage test

--- tests/homePageTest.spec.ts
+++ tests/homePageTest.spec.ts
@@ -6,17 +6,18 @@ import { createNewAContactFields } from '../E2E-TESTS/utils/contactForm';
 
 
 
+
 test('DemoBlaze Home Page test', async ({ page }) => {
     const buildTest = new BasePage(page);
     await buildTest.NavtoApp();
     const DBHomePage = new HomePage(page);
     await DBHomePage.navigateToHome();
     await DBHomePage.expectDemoBlazeLaunch();
-    const sndMsgData = createNewAContactFields();
+    const sndMsgData = createNewAContactFields
     const sendMsg = new ContactForm(page);
     await DBHomePage.navigateToContacts();
     await sendMsg.fillContactForm(sndMsgData);
-    await buildTest.closeApp();
+    await buildTest.closeApp
 
 
 
