8c18382434a9da58739ee557d601f86ef48eb1d5
Demoriel Purnell <demorielpurnell@Demoriels-MBP.attlocal.net>
2023-05-23 06:39:53 -0500

    order phone test

--- tests/samsungGalaxyS7OrderTest.spec.ts
+++ tests/samsungGalaxyS7OrderTest.spec.ts
@@ -8,7 +8,7 @@ import { createPlaceOrderFields } from '../E2E-TESTS/utils/placeOrderForm';
 
 
 
-test('DemoBlaze Phone Order Test', async ({ page }) => {
+test('DemoBlaze Samsung Galaxy S7 Order Test', async ({ page }) => {
     const buildTest = new BasePage(page);
     await buildTest.NavtoApp();
     const DBHomePage = new HomePage(page);
@@ -23,14 +23,14 @@ test('DemoBlaze Phone Order Test', async ({ page }) => {
     const orderForm = createPlaceOrderFields();
     const placeOrder = new PlaceOrder(page);
     await DBHomePage.navigateToPhones();
-    await placeOrder.clickIphone();
+    await placeOrder.clickSamsungGalaxyS7();
     await placeOrder.clickAddToCart();
     await placeOrder.handleDialog();
     await DBHomePage.navigateToCarts();
     
     await placeOrder.clickPlaceOrder();
     await placeOrder.fillPlaceOrderForm(orderForm);
-    await DBHomePage.expectIphoneOrder
-    await placeOrder.clickPurchase
+    await DBHomePage.expectPhoneOrder();
+    await placeOrder.clickPurchase();
    
 });
